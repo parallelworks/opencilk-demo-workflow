9449d82227acde9e916cc30eb13feb6bc15b6c71
Parallel Works app-run user <sfgary@mgmt-sfgary-awstest-00055.pw-canary-us-east-1.pw.local>
2023-03-09 16:11:37 +0000

    Change permissions, slight tweaks, add install timing

--- install.sh
+++ install.sh
@@ -9,18 +9,19 @@
 # Install gcc7.3.1, OpenCilk requires gcc5.1+
 sudo yum install -y devtoolset-7
 
-# Download CMake source
+# Download CMake source, OpenCilk requires 3.18+
 wget https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3.tar.gz
 tar -xvzf cmake-3.25.3.tar.gz
 
 # Build CMake
 cd cmake-3.25.3
-echo './bootstrap && make && sudo make install' | scl enable devtoolset-7 bash
+echo Building CMake...
+time echo './bootstrap && make && sudo make install' | scl enable devtoolset-7 bash
 
 # Download OpenCilk source
 git clone -b opencilk/v2.0 https://github.com/OpenCilk/infrastructure
 infrastructure/tools/get $(pwd)/opencilk
 
-# Build OpenCilk
-echo 'infrastructure/tools/build $(pwd)/opencilk $(pwd)/build' | scl enable devtoolset-7 bash
+echo Building OpenCilk...
+time echo 'infrastructure/tools/build $(pwd)/opencilk $(pwd)/build' | scl enable devtoolset-7 bash
 
