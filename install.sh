c85b73154a430064babbfa20c70ee07b47934c00
stefangary <40672572+stefangary@users.noreply.github.com>
2023-03-09 12:21:59 -0500

    Added specification of install_dir and permissions

--- install.sh
+++ install.sh
@@ -6,6 +6,15 @@
 # install CMake from source.
 #============================
 
+# Select where to install everything.
+# /var/lib/ is persistent on cloud images
+# $HOME, although persistent, is not a good choice
+# since there are permissions issues.
+install_dir="/var/lib/pworks"
+sudo mkdir -p $install_dir
+sudo chmod a+rwx --recursive $install_dir
+cd $install_dir
+
 # Install gcc7.3.1, OpenCilk requires gcc5.1+
 sudo yum install -y devtoolset-7
 
@@ -25,3 +34,5 @@ infrastructure/tools/get $(pwd)/opencilk
 echo Building OpenCilk...
 time echo 'infrastructure/tools/build $(pwd)/opencilk $(pwd)/build' | scl enable devtoolset-7 bash
 
+# Ensure all users have access
+sudo chmod a+rwx --recursive $install_dir
