9e86aaa81c8cb1514414d997ab93497cc206ccf0
Parallel Works app-run user <sfgary@mgmt-sfgary-awstest-00065.pw-canary-us-east-1.pw.local>
2023-03-10 05:32:18 +0000

    Adjust path in sample runner script

--- run_cilk_tests.sh
+++ run_cilk_tests.sh
@@ -8,7 +8,7 @@ git clone https://github.com/opencilk/tutorial
 
 # Compile code
 cd tutorial
-$HOME/build/bin/clang qsort.c -o qsort -O3 -fopencilk
+$HOME/pworks/build/bin/clang qsort.c -o qsort -O3 -fopencilk
 
 # Run code
 CILK_NWORKERS=8 ./qsort 100000
